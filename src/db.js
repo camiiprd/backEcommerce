fd3b637a52be83485b1cf2f16bcad22c0248f517
camiiprd <camila.prado.031@gmail.com>
2024-07-23 02:02:17 -0300

    back

---
+++ src/db.js
@@ -0,0 +1,18 @@
+import { createPool } from "mysql2/promise";
+import dotenv from 'dotenv';
+
+// Cargar las variables de entorno del archivo .env
+dotenv.config();
+
+export const pool = createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+pool.getConnection((error, connection) => {
+    if (error) throw error;
+    console.log('Conectada correctamente', connection);
+});
